b7689b89f42f042cd3c217d1e46ddc7b992bbdf8
CoderCakee <genereyes2001@gmail.com>
2025-10-13 23:43:13 +0800

    Project Initiated

--- main_project/fast_track_backend/fast_track_backend/urls.py
+++ main_project/fast_track_backend/fast_track_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fast_track project.
+URL configuration for fast_track_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- main_project/fast_track_backend/manage.py
+++ main_project/fast_track_backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fast_track.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fast_track_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
